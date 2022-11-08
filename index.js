d997a31053808d5504877e9f4bd8736a225326c1
mohaimenur1 <mmrahmanrahi@gmail.com>
2022-11-08 22:46:40 +0600

    making a get route to read services data

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
 
@@ -7,6 +8,42 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const uri =
+  "mongodb+srv://rahi:MLM4HtxSaGKzxXAO@cluster0.ap4ff9h.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const serviceCollection = client.db("visaration").collection("services");
+
+    app.get("/services", async (req, res) => {
+      const cursor = serviceCollection.find({});
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    //   app.post("/users", async (req, res) => {
+    //     console.log("Post API called");
+    //     const user = req.body;
+
+    // users.push(user);
+    // console.log(user);
+    // const result = await userCollection.insertOne(user);
+    // console.log(result);
+    // user._id = result.insertedId;
+    // res.send(user);
+    // console.log(req.body);
+    //   });
+  } finally {
+  }
+}
+
+run().catch((err) => console.log(err));
 
 const port = process.env.PORT || 5000;
 
