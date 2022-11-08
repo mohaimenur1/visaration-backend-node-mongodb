b7623355052ff2ed1364441405ce91ece5a2979c
mohaimenur1 <mmrahmanrahi@gmail.com>
2022-11-08 22:43:08 +0600

    port call for running the server

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`port running on port ${port}`);
+});
