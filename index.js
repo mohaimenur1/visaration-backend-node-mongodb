4d72c9eba8dec2ab5312facda4780059dca0d7c5
mohaimenur1 <mmrahmanrahi@gmail.com>
2022-11-10 21:12:01 +0600

    delete route

--- index.js
+++ index.js
@@ -92,6 +92,7 @@ async function run() {
       res.send(result);
     });
 
+    // delete route
     app.delete("/reviews/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
